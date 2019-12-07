510aacaeff3b12ee11ee8706c6d03bb7aedbaac5
Fabio Bocampagni <a.bocampagni@gmail.com>
2019-12-07 20:02:09 -0300

    Calculadora simples pt.1

---
+++ src/application/contador.java
@@ -0,0 +1,24 @@
+package application;
+
+public class contador {
+	
+	public contador() {}
+	
+	
+	public double soma(double x, double y) {
+		return x + y;
+	}
+	
+	public double sub(double x, double y) {
+		return x - y;
+	}
+	
+	public double produto(double x, double y) {
+		return x * y;
+	}
+	
+	//Quem vai tratar é a main
+	public double div(int x, int y) {
+		return x / y;
+	}
+}
--- src/application/main.java
+++ src/application/main.java
@@ -1,8 +1,46 @@
 package application;
 
+import java.util.Scanner;
+
 public class main {
 	public static void main(String[] args) {
-
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			contador cont = new contador();
+			
+			
+			System.out.println("Escolha a operação: ");
+			System.out.println("1 - Soma\n2 - Subtração\n3 - Produto\n4 - divisão");
+			int escolha = sc.nextInt();
+			
+			
+			
+			if(escolha == 1) {
+				double resultado = cont.soma(x, y);
+				System.out.println(resultado);
+			}
+			else if(escolha == 2) {
+				double resultado = cont.sub(x, y);
+				System.out.println(resultado);
+			}
+			else if(escolha == 3) {
+				double resultado = cont.produto(x, y);
+				System.out.println(resultado);
+			}
+			else if(escolha == 4) {
+				try {
+					cont.div(x, y);
+				}
+				catch(ArithmeticException e) {
+					System.out.println("Divisão por zero");				
+				}
+			}
+			else {
+				System.out.println("Opção inválida");
+			}
+			
+		}
 	}
-
 }
